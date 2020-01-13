3ac43136408fb15f332b94dc13cc4fa274511f79
alex-ismailov <alexander.is.dev@gmail.com>
2020-01-14 00:38:53 +0300

    added: comments

--- src/sort bubble.js
+++ src/sort bubble.js
@@ -1,9 +1,9 @@
 const bubbleSort = (arr) => {
   const len = arr.length;
   const locArr = arr;
-  for (let i = 0; i < len; i += 1) {
-    for (let j = 0; j < len - i; j += 1) {
-      if (locArr[j] > locArr[j + 1]) {
+  for (let i = 0; i < len; i += 1) { // проходимся по каждому элементу массива
+    for (let j = 0; j < len - i; j += 1) { // сравниваем текущ. элемент с оставшимися, уменьшая при этом длину проходки с конца массива
+      if (locArr[j] > locArr[j + 1]) { // если текущий элемент меньше следующего, то меняем их местами
         const tmp = locArr[j];
         locArr[j] = locArr[j + 1];
         locArr[j + 1] = tmp;
