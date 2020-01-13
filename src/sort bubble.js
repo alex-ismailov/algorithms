e09d63ef10fd7fb37f441357f6cd1478ab0f2bb4
alex-ismailov <alexander.is.dev@gmail.com>
2020-01-13 23:41:20 +0300

    added: bubbleSort function

---
+++ src/sort bubble.js
@@ -0,0 +1,17 @@
+const bubbleSort = (arr) => {
+  const len = arr.length;
+  for (let i = 0; i < len; i += 1) {
+    for (let j = 0; j < len - i; j += 1) {
+      if (arr[j] < arr[j + 1]) {
+        let tmp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tmp;
+      }
+    }
+  }
+  return arr;
+};
+
+// testing
+const arr = [9, 3, 8, 5, 6, 1, 4, 2, 7];
+console.log(bubbleSort(arr));
