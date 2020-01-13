040d373c8fc4f079034ed6d764fef48c0479fe75
alex-ismailov <alexander.is.dev@gmail.com>
2020-01-13 23:43:33 +0300

    change to increasing sorting

--- src/sort bubble.js
+++ src/sort bubble.js
@@ -2,7 +2,7 @@ const bubbleSort = (arr) => {
   const len = arr.length;
   for (let i = 0; i < len; i += 1) {
     for (let j = 0; j < len - i; j += 1) {
-      if (arr[j] < arr[j + 1]) {
+      if (arr[j] > arr[j + 1]) {
         let tmp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = tmp;
