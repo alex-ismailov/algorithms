eab6ccf3489e3e1d6a51e88b4908a406b70d55f5
alex-ismailov <alexander.is.dev@gmail.com>
2020-01-19 00:23:48 +0300

    added: bubbleSort(optimized) hexlet ver.js

---
+++ src/sort bubble hexlet ver.js
@@ -0,0 +1,21 @@
+const bubbleSort = (arr) => {
+  let size = arr.length;
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < size; i += 1) {
+      if (arr[i] > arr[i + 1]) {
+        let tmp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = tmp;
+        swapped = true;
+      }
+    }
+    size -= 1;
+  } while (swapped);
+
+  return arr;
+};
+
+// testing
+console.log(bubbleSort([3, 2, 10, -2, 0]));
