3b8d723b94bb68ff2e97b6d35476c2bc0a33e481
alex-ismailov <alexander.is.dev@gmail.com>
2020-01-19 18:36:13 +0300

    added: isBracketStructureBalanced function ver 1

---
+++ src/isBracketStructureBalanced ver 1.js
@@ -0,0 +1,26 @@
+const openBrackets = ['(', '{', '[', '<'];
+const pairBrackets = ['()', '{}', '[]', '<>'];
+
+const isBracketStructureBalanced = (expression) => {
+  const stack = [];
+  for (const currBracket of expression) {
+    if (openBrackets.includes(currBracket)) { // если текущая скобка открывающая
+      stack.push(currBracket);
+    } else { // если текущая скобка закрывающая
+      const prevBracket = stack.pop();
+      const currPairBrackets = `${prevBracket}${currBracket}`;
+      if (!pairBrackets.includes(currPairBrackets)) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
+
+// testing
+console.log(isBracketStructureBalanced('[()]'));       // true
+console.log(isBracketStructureBalanced('{<>}}'));      // false
+console.log(isBracketStructureBalanced('['));          // false
+console.log(isBracketStructureBalanced('}{'));         // false
+console.log(isBracketStructureBalanced('(([<>]){})')); // true
+console.log(isBracketStructureBalanced('([{]})'));     // false
